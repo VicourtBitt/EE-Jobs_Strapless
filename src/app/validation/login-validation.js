4247ef70ec7726764127ffdfbec3fc4143814a92
VicourtBitt <victorgcb@outlook.com>
2024-08-01 00:43:28 -0300

    Adding some comments and hoping for the best

--- src/app/validation/login-validation.js
+++ src/app/validation/login-validation.js
@@ -6,7 +6,12 @@ const signInputs = document.querySelectorAll("#formulary-signin > input")
 const invalidHints = document.querySelectorAll(".hint-invalid")
 
 
+// Hello Teacher Steffany, I'm only writing this in english because it
+// seems better (in my opinion at least), and because it seems right when
+// we talk about documentation (even if my code seems worse than the "Tiête")
+
 function validateElem (elem) {
+    // This function is called whenever it should validate a element
     if (elem.id == "loginEmail" || elem.id == "signEmail") {
         let isEmailValid = validateEmail(elem.value)
         if (isEmailValid) {
@@ -21,8 +26,10 @@ function validateElem (elem) {
 // EVENT LISTENERS TO INPUTS
 
 function addListener (list) {
+    // This function adds a event listener to every element we pass
+    // in the function call, so we can pass a list of elems
     list.forEach((elem) => {
-        elem.addEventListener("blur", () => {
+        elem.addEventListener("keydown", () => {
             validateElem(elem)
         })
     })
@@ -30,7 +37,11 @@ function addListener (list) {
 
 // EVENT LISTENERS TO FORMULARIES
 // EVENT LISTENERS TO FORMULARIES
+
 function formListener (elem) {
+    // This is the event specific event listener for the formulary
+    // Basically we are only gathering the values to see if the code
+    // is actuaçy working as intended
     elem.addEventListener("submit", (e) => {
         e.preventDefault()
         if (elem.id == "formulary-signin") {
@@ -48,6 +59,8 @@ function formListener (elem) {
 // REGEX EXPRESSIONS
 
 function validateEmail (emailValue) {
+    // Seems quite self-explanatory. This function test the regular
+    // expression in the email that it receives
     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(emailValue)
 }
 
@@ -55,6 +68,8 @@ function validateEmail (emailValue) {
 // ADD VALIDATION HINT RESULTS
 
 function fieldIsValid (elem) {
+    // It just alters the input validity state (in the hard way),
+    // not using setCustomValidity. °
     const spanHint = elem.previousElementSibling.children[1]
     spanHint.classList.add("hidden-hint")
     elem.classList.remove("input-invalid")
@@ -62,6 +77,8 @@ function fieldIsValid (elem) {
 }
 
 function fieldNotValid (elem) {
+    // It just alters the input validity state (in the hard way),
+    // not using setCustomValidity. °°
     const spanHint = elem.previousElementSibling.children[1]
     spanHint.innerText = "Entrada Inválida"
     spanHint.classList.remove("hidden-hint")
@@ -71,6 +88,8 @@ function fieldNotValid (elem) {
 }
 
 function removePrevious (elem) {
+    // tbh, i cannot remember what this function has even meant
+    // to be one day. If I remember, then I will change this..
 }
 
 addListener(loginInputs)

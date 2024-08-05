0d17663c2853f1790826c660ec969b350a3bad82
VicourtBitt <victorgcb@outlook.com>
2024-08-05 14:52:14 -0300

    fixing styles

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ import express from "express"
 
 const app = express()
 
-const PORT = 3001
+const PORT = 3000
 
-app.use(express.static("public"))
+app.use(
+    express.static("public"),
+    express.static("public/validation")
+    )
 
 app.get("/", (req, res) => {
     res.sendFile( "public", "index.html")
 })
 
 app.listen(PORT, () => {
-    console.log("Servidor rodando.")
+    console.log("Servidor rodando na porta http://localhost:3000.")
 })
--- public/validation/contact-us-validation.js
+++ public/validation/contact-us-validation.js
@@ -1,8 +1,7 @@
-import { validateName, validateEmail, validateSelection } from "./validations.js"
-import { addListener } from "./eventListeners.js"
+import { addListener } from "./eventListeners.js";
 
-const companyInputs = document.querySelectorAll("#company-formulary > input")
-const companySelect = document.querySelectorAll("#company-formulary > select")
+const companyInputs = document.querySelectorAll("#company-formulary > input");
+const companySelect = document.querySelectorAll("#company-formulary > select");
 
-addListener(companySelect, "mouseup")
-addListener(companyInputs, "keydown")
+addListener(companySelect, "mouseup");
+addListener(companyInputs, "keydown");

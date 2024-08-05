5a049d4335d01a7e31f4e60f9ae74ecde9474fe9
VicourtBitt <victorgcb@outlook.com>
2024-08-05 03:59:09 -0300

    Revert "add featuring new interactions and form validations 2"
    This reverts commit a0102b86db2ac346018c646eba35cc3efc347cf6.

--- public/validation/contact-us-validation.js
+++ public/validation/contact-us-validation.js
@@ -1,3 +1,4 @@
+import { validateName, validateEmail, validateSelection } from "./validations.js"
 import { addListener } from "./eventListeners.js"
 
 const companyInputs = document.querySelectorAll("#company-formulary > input")

17a4192431a1aefd9f40e6cb2de079c7b2f32ba8
VicourtBitt <victorgcb@outlook.com>
2024-08-09 01:34:34 -0300

    testing fetch

--- database.js
+++ database.js
@@ -2,6 +2,6 @@ const appDB = require("./src/api/appDB.js")
 
 const PORT = 3001
 
-appDB.listen(PORT, '0.0.0.0',() => {
+appDB.listen(PORT, () => {
     console.log(`Banco de dados ativo e monitorando requisições HTTP na porta http://localhost:${PORT}`)
 })
